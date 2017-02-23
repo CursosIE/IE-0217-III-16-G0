18d96cb3374ce5a6be479c4bd67999754a2e7b06
danieljimenezl <daniel08leon@gmail.com>
2017-02-23 14:35:28 -0600

    Clase Edge

--- Laboratorio3/graph/Edge.h
+++ Laboratorio3/graph/Edge.h
@@ -5,9 +5,28 @@
 
 using namespace std;
 
-template <typename D, typename P>
+template <typename D>
 class Edge {
+public:
+    Node<D>* From;
+    Node<D>* To;
+    double Weight;
 
+    Edge() {
+        From = nullptr;
+        To = nullptr;
+    }
+
+    Edge(Node<D>* From, Node<D>* To, double W) {
+        this->From = From;
+        this->To = To;
+        this->Weight = W;
+    }
+
+    virtual ~Edge() {
+        delete From;
+        delete To;
+    }
 };
 
 #endif /* EDGE_H */
