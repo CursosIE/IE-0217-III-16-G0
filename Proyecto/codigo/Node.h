fc080a32ccafc8998d6ee124349808c823df3e4d
Luis Diego Fernandez Coto <luisdifc1993@gmail.com>
2017-03-06 00:32:06 -0600

    se agregaron comentarios

--- Proyecto/codigo/Node.h
+++ Proyecto/codigo/Node.h
@@ -1,20 +1,32 @@
+/**
+*@file Node.h
+*@version 1.0
+*@date 6/03/17
+*@author Luis Diego Fernandez, Daniel Jimenez
+*@title Arbol B+
+*@brief Clase Nodo 
+*/
+
 #ifndef NODE_H
 #define NODE_H
 
 #include <iostream>
 using namespace std;
-//B+Tree node
 
+//se crea la clase como una plantilla
 template <typename Data>
 class Node {
 public:
-    int elements;
-    int order;
-    int isLeaf;
-    int* arrayKeys;
-    Node<Data>** arrayPtrs;
-    Node<Data>* father;
+    int elements; //elementos del nodo
+    int order; //orden del arbol
+    int isLeaf; //bandera de control, en este caso es falsa 
+    int* arrayKeys; //arreglo de llaves 
+    Node<Data>** arrayPtrs; //arrelgo de punteros
+    Node<Data>* father; //puntero al padre
 
+
+    /*! \brief Constructor por defecto.
+     */
     Node() {
         order = 0;
         this->father = nullptr;
@@ -24,6 +36,8 @@ public:
         this->elements = 0;
     }
 
+    /*! \brief Constructor para crear un Nodo de un tamanno pre establecido.
+     */
     Node(int order) {
         this->father = nullptr;
         this->order = order;
@@ -38,6 +52,8 @@ public:
         this->elements = 0;
     }
 
+    /*! \brief Constructor por copia.
+     */
     Node(int order, Node<Data>* father) {
         this->order = order;
         this->father = father;
@@ -46,6 +62,8 @@ public:
         isLeaf = 0;
     }
 
+    /*! \brief Destructor.
+     */
     virtual ~Node() {
         delete[] arrayKeys;
         delete[] arrayPtrs;
